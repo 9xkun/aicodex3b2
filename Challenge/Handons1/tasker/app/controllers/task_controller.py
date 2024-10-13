e75630b173540a98a01cd46359b24299030eb5bc
pthoang <phantichhoang@gmail.com>
2024-10-13 13:56:41 +0700

    feat: hld

--- Challenge/Handons1/tasker/app/controllers/task_controller.py
+++ Challenge/Handons1/tasker/app/controllers/task_controller.py
@@ -1,20 +1,20 @@
 from flask import Blueprint, request, jsonify
 from app.models.task_model import Task
 
-task_bp = Blueprint('task_api', __name__)
+task_bp = Blueprint('task_api', __name__, url_prefix='/tasks')
 
-@task_bp.route('/tasks', methods=['GET'])
+@task_bp.route('/', methods=['GET'])
 def get_tasks():
     tasks = Task.get_all_tasks()
     return jsonify(tasks)
 
-@task_bp.route('/tasks', methods=['POST'])
+@task_bp.route('/', methods=['POST'])
 def create_task():
     data = request.get_json()
     task = Task.create_task(data)
     return jsonify(task)
 
-@task_bp.route('/tasks/<int:task_id>', methods=['GET'])
+@task_bp.route('/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = Task.get_task_by_id(task_id)
     if task:
@@ -22,7 +22,7 @@ def get_task(task_id):
     else:
         return jsonify({'message': 'Task not found'}), 404
 
-@task_bp.route('/tasks/<int:task_id>', methods=['PUT'])
+@task_bp.route('/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     data = request.get_json()
     task = Task.update_task(task_id, data)
@@ -31,7 +31,7 @@ def update_task(task_id):
     else:
         return jsonify({'message': 'Task not found'}), 404
 
-@task_bp.route('/tasks/<int:task_id>', methods=['DELETE'])
+@task_bp.route('/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
     result = Task.delete_task(task_id)
     if result:
